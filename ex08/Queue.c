8f44c8279432f261057fa61f578761c75b018fc2
Joost-vanAndel <joost-vanandel@live.nl>
2020-07-08 01:14:21 +0200

    multi-threaded implementation

--- ex08/Queue.c
+++ ex08/Queue.c
@@ -117,16 +117,16 @@ void showQueue(const queue_t* queue) {
 }
 
 // Print the data/nodes in a queue to a file:
-void writeQueueToFile(const queue_t* queue, FILE *fp){
+void writeQueueToFile(const queue_t* queue,const FILE *fp){
   const node_t* nextN = queue->lastNode;
   if(nextN == NULL) {
-    fprintf(fp, "Queue is empty, last node is %p\n", (void*) queue->lastNode);
+    fprintf(&fp, "Queue is empty, last node is %p\n", (void*) queue->lastNode);
   } else {
-    fprintf(fp, "Queue contains %ld nodes:\n", sizeQueue(queue));
-    fprintf(fp, "Last node: %p\n", (void*) queue->lastNode);
+    fprintf(&fp, "Queue contains %ld nodes:\n", sizeQueue(queue));
+    fprintf(&fp, "Last node: %p\n", (void*) queue->lastNode);
     do {
       nextN = nextN->nextNode;
-      fprintf(fp, "pNode = %p  Data = '%d' '%s'  nextN = %p\n",
+      fprintf(&fp, "pNode = %p  Data = '%d' '%s'  nextN = %p\n",
              (void*)nextN, nextN->data.intVal, nextN->data.text, (void*)nextN->nextNode);
     } while(nextN != queue->lastNode);
   }
--- ex08/sharedQueue.c
+++ ex08/sharedQueue.c
@@ -10,74 +10,93 @@
 #include "Queue.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-void producer(queue_t *queue, data_t data, int interval);
-void consumer(queue_t *queue, data_t data, int interval);
+void *producer(void *arguments);
+void *consumer(void *arguments);
+
+struct thread_args {
+  queue_t *queue;
+  data_t data;
+  int interval;
+};
 
 int main() {
-  data_t data = {1, "Hello queue"};
+  pthread_t producerA, producerB, producerC, consumerA;
   queue_t queue = {NULL};  // Note: element of queue = NULL
-  
-  printf("\nList the contents of the current queue:\n");
-  showQueue(&queue);
-  
-  printf("\nCreate a new queue:\n");
-  createQueue(&queue, data);
-  showQueue(&queue);
-  data.intVal++;
-  
-  printf("\nAdd new data to the queue:\n");
-  pushQueue(&queue, data);
-  showQueue(&queue);
-  data.intVal++;
-  
-  printf("\nAdd new data to the queue:\n");
-  pushQueue(&queue, data);
-  showQueue(&queue);
-  data.intVal++;
-  
-  printf("\nAdd new data to the queue:\n");
-  pushQueue(&queue, data);
-  showQueue(&queue);
-  
-  printf("\nRemove data from the queue:\n");
-  popQueue(&queue);
-  showQueue(&queue);
-  
-  printf("\nFront iValue/first node: %d\n", frontQueue(&queue)->intVal);
-  printf("Back  iValue/last node:  %d\n", backQueue(&queue)->intVal);
-  
-  printf("\nDelete the current queue:\n");
-  deleteQueue(&queue);
-  showQueue(&queue);
-  
-  printf("\nCreate a new queue:\n");
-  createQueue(&queue, data);
-  showQueue(&queue);
-  data.intVal++;
+  struct thread_args argsP_A = {NULL}, argsP_B = {NULL}, argsP_C = {NULL}, argsC_A = {NULL};
+
+  printf("setting arguments\n");
+  argsP_A.queue = &queue;
+  argsP_B.queue = &queue;
+  argsP_C.queue = &queue;
+  argsC_A.queue = &queue;
+
+  argsP_A.data.text[20] = "producer A";
+  argsP_A.data.intVal = 1;
+  argsP_B.data.text[20] = "producer B";
+  argsP_B.data.intVal = 2;
+  argsP_C.data.text[20] = "producer C";
+  argsP_C.data.intVal = 3;
+  //argsP_A.data = {1, "this is producer A"};
+  //argsP_B.data = {2, "this is producer B"};
+  //argsP_C.data = {3, "this is producer C"};
+  //argsC_A.data = {5, "this is consumer A"};
+
+  argsP_A.interval = 2;
+  argsP_B.interval = 3;
+  argsP_C.interval = 4;
+  argsC_A.interval = 15;
+
+  printf("creating threads\n");
+  pthread_create(&producerA, NULL, producer, (void *) &argsP_A);
+  pthread_create(&producerB, NULL, producer, (void *) &argsP_B);
+  pthread_create(&producerC, NULL, producer, (void *) &argsP_C);
+  pthread_create(&consumerA, NULL, consumer, (void *) &argsC_A);
+
+  printf("threads created\n");
+
+  pthread_join(producerA, NULL);
+  pthread_join(producerB, NULL);
+  pthread_join(producerC, NULL);
+  pthread_join(consumerA, NULL);
   
   return 0;
 }
 
-void producer(queue_t *queue, data_t data, int interval)
+void *producer(void *arguments)
 {
+  struct thread_args *args = (struct thread_args*)arguments;
+  printf("new producer %d\n", args->data.intVal);
   while(1)
     {
-      pushQueue(&queue, data);
-      sleep(interval);
+      printf("producer %d pushing\n", args->data.intVal);
+      pushQueue(&args->queue, args->data);
+      sleep(args->interval);
     }
   return;
 }
-void consumer(queue_t *queue, data_t data, int interval)
+void *consumer(void *arguments)
 {
+  printf("new consumer\n");
+  struct thread_args *args = (struct thread_args*)arguments;
+
+  printf("open file\n");
   FILE *fp;
   fp = fopen ("queueFile.txt", "a");
-
+  if(fp = NULL)
+    {
+      printf("file failed to open\n");
+      exit(1);
+    }
   while(1)
     {
-      writeQueueToFile(&queue, &fp);
-      showQueue(&queue);
-      sleep(interval);
+      printf("now consuming\n");
+      writeQueueToFile(&args->queue, &fp);
+      showQueue(&args->queue);
+      emptyQueue(&args->queue);
+      sleep(args->interval);
     }
   fclose(fp);
   return;
